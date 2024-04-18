5f7beb8db5ab99911db4dcfc8724072dfaa6ee52
Firestone82 <mikpav@email.cz>
2024-04-19 00:07:42 +0200

    Todays work
    Took 4 hours 1 minute

---
+++ mik0486/src/sourceCodes/DataRequestComponent.ts
@@ -0,0 +1,37 @@
+export class Data {
+    constructor(
+        private root: HTMLElement,
+        private template: string,
+    ) {}
+
+    public queryData(url: string, lazy: boolean = false, include: [] = []): void {
+        const currentTime = new Date().getTime();
+
+        const Notiflix = window['Notiflix'];
+        Notiflix.Block.circle("#entryDataReplacement", 'Loading...');
+
+        const request = new XMLHttpRequest();
+        request.onreadystatechange = () => {
+            if (request.readyState === 4 && request.status === 200) {
+                const took = new Date().getTime() - currentTime;
+                console.log(`Data | Query took ${took}ms`);
+
+                // Replace the object data
+                const tableDataElement = this.root.querySelector('#entryDataReplacement');
+                tableDataElement.innerHTML = request.responseText;
+
+                // Evaluate the script
+                const dataScript = document.querySelector('#entryDataScript');
+                if (dataScript) eval(dataScript.innerHTML);
+            }
+        };
+
+        let newUrl = url + `?template=${this.template}&lazy=${lazy}`
+        newUrl += include.length ? `&include=${include.join(',')}` : '';
+
+        console.log(`Data | Querying data from ${url}`)
+        console.log(`Data |  - Params: ${newUrl.replace(url, '')}`);
+        request.open('GET', newUrl, true);
+        request.send();
+    }
+}
